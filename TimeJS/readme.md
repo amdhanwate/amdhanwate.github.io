bc950ed77895d56a6dec07dfb13952e8b8760254
amdhanwate <abhijeet@rhythmflows.com>
2026-08-10 00:47:37 +0530

    updated postfolio

--- TimeJS/readme.md
+++
@@ -1,1 +0,0 @@
-REPO FOR TIME.JS
--- texteditor/main.js
+++
@@ -1,9 +0,0 @@
-document.getElementsByTagName('iframe')[0].focus();
-
-function enableDesignMode(){
-    editor.document.designMode = 'On';
-}
-
-function execCmd(command, arg=null){
-    editor.document.execCommand(command, false, arg);
-}
